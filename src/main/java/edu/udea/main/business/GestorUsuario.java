7082844fa84a153f591c725d397d1e282863a19c
Oscar Andres Caicedo Bedoya <oscar.caicedo@frubana.com>
2022-08-24 08:58:08 -0500

    Clase 24 de Agosto

--- src/main/java/edu/udea/main/business/GestorUsuario.java
+++ src/main/java/edu/udea/main/business/GestorUsuario.java
@@ -13,6 +13,17 @@ public class GestorUsuario {
         this.usuarios = new ArrayList<>();
 
         this.usuarios.add(new Usuario("Prueba1", "Prueba","1234"));
+        this.usuarios.add(new Usuario("Prueba1", "Andres","1234"));
+    }
+
+
+    public Usuario getUsuario(String nombreUsuario) throws Exception {
+        for(Usuario usuario: this.usuarios){
+            if(usuario.getNombreUsuario().equals(nombreUsuario) ){
+                    return usuario;
+            }
+        }
+        throw new Exception("Usuario No Existe");
     }
 
     public ArrayList<Usuario> getUsuarios() {
--- src/main/java/edu/udea/main/controlador/UsuarioControlador.java
+++ src/main/java/edu/udea/main/controlador/UsuarioControlador.java
@@ -5,8 +5,8 @@ import edu.udea.main.business.GestorUsuario;
 import edu.udea.main.model.Usuario;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
@@ -15,9 +15,25 @@ public class UsuarioControlador {
 
     private GestorUsuario gestorUsuario = new GestorUsuario();
 
-    @GetMapping("/usuario")
+    @GetMapping("/usuarios")
     public ResponseEntity<ArrayList<Usuario>> getUsuarios(){
-        return new ResponseEntity<>(gestorUsuario.getUsuarios(), HttpStatus.ACCEPTED);
+        return new ResponseEntity<>(gestorUsuario.getUsuarios(), HttpStatus.OK);
+    }
+
+    @GetMapping("/usuario")
+    public ResponseEntity<Object> getUsuario(@RequestParam String id){
+        try {
+            Usuario usuario = gestorUsuario.getUsuario(id);
+            return new ResponseEntity<>(usuario,HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+    }
+
+    @GetMapping("/usuario/{id}")
+    public ResponseEntity<String> getUsuarioPath(@PathVariable String id){
+        return new ResponseEntity<>(id,HttpStatus.OK);
     }
 
 }
